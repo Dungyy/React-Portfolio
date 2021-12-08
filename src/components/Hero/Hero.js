7f5ef473a8311aa4d2290e1d7940c8d21b4a9f0e
Dungyy <erickmunoz13@gmail.com>
2021-12-08 01:02:59 -0600

    fixed some aos

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,11 +13,11 @@ const Hero = () => {
 return(
   <Section row nopadding>
     <LeftSection>
-      <SectionTitle main center data-aos="fade-down" data-aos-easing="linear"data-aos-duration="1900">
+      <SectionTitle main center data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1700">
         Hello,
         Welcome to My Portfolio
       </SectionTitle>
-      <SectionText data-aos="fade-up"data-aos-duration="3500">
+      <SectionText data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
         I'm a full-stack Web Developer with expertise in ReactJS for frontend, MongoDB as my database, NodeJS for backend along with ExpressJS for making HTTPS and API requests.
       </SectionText>
       <Button onClick={() => window.location = 'mailto:erickmunoz13@gmail.com'}>Contact Me</Button>
