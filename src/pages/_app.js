35015cf4399cda9dc2d008e7219c0d5594a0229d
Dungyy <erickmunoz13@gmail.com>
2022-01-24 03:40:04 -0600

    Serve Build

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Dungy's React Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Theme>
         <Component {...pageProps} />
